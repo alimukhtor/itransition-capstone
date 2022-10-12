46f8f3624256bebc43ae334ebb18e5551d6e6440
Izzatillo <izzatillo.mukhtorov@ydistri.com>
2022-10-12 11:39:05 +0500

    fetch collections

--- src/components/Admin/Collections.jsx
+++ src/components/Admin/Collections.jsx
@@ -6,30 +6,29 @@ const Collections = () => {
   const [collections, setCollections] = useState([]);
   const token = window.localStorage.getItem("token");
   useEffect(() => {
-    fetchUsers();
-  });
-  const fetchUsers = async () => {
-    const response = await fetch(
-      "https://itransition-capstone.herokuapp.com/collections/allCollections",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + token,
-        },
-      }
-    );
-    const data = await response.json();
-    setCollections(data);
-    console.log("Collections", data);
-  };
-
+    const fetchUsers = async () => {
+      const response = await fetch(
+        "https://itransition-capstone.herokuapp.com/collections/allCollections",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token,
+          },
+        }
+      );
+      const data = await response.json();
+      setCollections(data);
+      console.log("Collections", data);
+    };
+    fetchUsers()
+  },[]);
   console.log("Col", collections);
   return (
     <Row>
       {collections.map((collection) => (
-        <Col md={6} lg={3} className="">
-          <Card className="card">
+        <Col md={6} lg={3} key={collection._id}>
+          <Card className="card border-0">
             <Link to={`singleCollection/${collection._id}`}>
             <Card.Img variant="top" src={collection.image} />
             <Card.Body className="card_body">
